9123480f6990ad1027ff27204bc57665adfde428
alaafkk <anfb1@alu.ua.es>
2026-03-06 10:31:45 +0100

    adelanto

--- Reproducibles.py
+++ Reproducibles.py
@@ -3,6 +3,8 @@ class ElementoMultimedia:
         self.titulo=titulo
         self.duracion=duracion
         self.fecha_lanzamiento=fecha_lanzamiento
+    def mostrar(self):
+       print(f'Estas reproduciendo {titulo} lanzada el {fecha_lanzamiento} y de duracion {duracion}')
 
 class Cancion(ElementoMultimedia):
     def __init__(self,titulo,duracion,fecha_lanzamiento,artista,album,genero):
@@ -10,13 +12,19 @@ class Cancion(ElementoMultimedia):
         self.artista = artista
         self.album = album
         self.genero = genero
+    def mostrar(self):
+        super().mostrar()
+        print(f'Del artista {artista} y el album {album}, Genero: {genero}')
+
 class Podcast(ElementoMultimedia):
     def __init__(self,titulo,duracion,fecha_lanzamiento,presentador,num_episodio,tema):
         super().__init__(titulo, duracion, fecha_lanzamiento)
         self.presentador = presentador
         self.num_episodio = num_episodio
         self.tema = tema
-
+    def mostrar(self):
+        super().mostrar()
+        print(f'Episodio {num_episodio} sobre {tema} del presentador {presentador}')
 
 
 
