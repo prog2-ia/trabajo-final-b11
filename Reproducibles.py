0113bd1037e0385a3aaaaf7a12cd09cc9e9e5f14
alaafkk <anfb1@alu.ua.es>
2026-03-20 11:23:16 +0100

    adelanto

--- Reproducibles.py
+++ Reproducibles.py
@@ -1,10 +1,10 @@
 class ElementoMultimedia:
-    def __init__(self,titulo,duracion,fecha_lanzamiento):
+    def __init__(self,titulo:str,duracion,fecha_lanzamiento):
         self.titulo=titulo
         self.duracion=duracion
         self.fecha_lanzamiento=fecha_lanzamiento
     def mostrar(self):
-       print(f'Estas reproduciendo {self.titulo} lanzada el {self.fecha_lanzamiento} y de duracion {self.duracion}')
+       return f'Estas reproduciendo {self.titulo} lanzada el {self.fecha_lanzamiento} y de duracion {self.duracion}'
 
 class Cancion(ElementoMultimedia):
     def __init__(self,titulo,duracion,fecha_lanzamiento,artista,album,genero):
@@ -13,8 +13,8 @@ class Cancion(ElementoMultimedia):
         self.album = album
         self.genero = genero
     def mostrar(self):
-        super().mostrar()
-        print(f'Del artista {self.artista} y el album {self.album}, Genero: {self.genero}')
+        mostrar_elemento=super().mostrar()
+        return f'{mostrar_elemento} del artista {self.artista} y el album {self.album}, Genero: {self.genero}'
 
 class Podcast(ElementoMultimedia):
     def __init__(self,titulo,duracion,fecha_lanzamiento,presentador,num_episodio,tema):
@@ -23,8 +23,8 @@ class Podcast(ElementoMultimedia):
         self.num_episodio = num_episodio
         self.tema = tema
     def mostrar(self):
-        super().mostrar()
-        print(f'Episodio {self.num_episodio} sobre {self.tema} del presentador {self.presentador}')
+        mostrar_elemento=super().mostrar()
+        return f'{mostrar_elemento} episodio {self.num_episodio} sobre {self.tema} del presentador {self.presentador}'
 class Playlist:
     def __init__(self,nombre_playlist):
         self.nombre_playlist=nombre_playlist
@@ -39,14 +39,27 @@ class Playlist:
             duracion_t+=cancion.duracion
         return duracion_t
     def mostrar_playlist(self):
-        print(f'Nombre de la playlist: {self.nombre_playlist}')
+        pistas_playlist=''
         for pista in self.pistas:
-            pista.mostrar_info()
+            pistas_playlist+=f'{pista.mostrar()}\n'
+        return f'Nombre de la playlist: {self.nombre_playlist}\n Canciones: \n{pistas_playlist}'
+class Biblioteca:
+    def __init__(self, nombre_biblioteca):
+        self.nombre = nombre_biblioteca
+        self.catalogo: list = []
+        self.playlists: list = []
 
+    def agregar_al_catalogo(self, elemento):
+        self.catalogo.append(elemento)
+        return f'{elemento.titulo} se ha añadido al catálogo general.'
 
+    def crear_playlist(self, nombre_playlist: str):
+        nueva_playlist = Playlist(nombre_playlist)
+        self.playlists.append(nueva_playlist)
+        return nueva_playlist
 
-
-
-
-
-
+    def mostrar_catalogo(self):
+        elementos_biblioteca=''
+        for elemento in self.catalogo:
+            elementos_biblioteca+=f'{elemento.mostrar()}\n'
+        return elementos_biblioteca
