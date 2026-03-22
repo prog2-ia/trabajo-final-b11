86a0efc08893b6b300fed13a227bfb761acc56d5
alaafkk <anfb1@alu.ua.es>
2026-03-23 00:52:33 +0100

    En este archivo he creado la clase Playlist para poder gestionar las listas de reproducción. Funciona de manera muy sencilla: empieza con un nombre y una lista vacía donde ir guardando los audios. Le he puesto las funciones básicas para añadir pistas (olvide comprobar si estaban o no en la pista pero despues lo corrigió mi compañero que lo explicará) y para eliminarlas si hace falta. También he incluido un método que recorre toda la lista sumando los tiempos para calcular la duración total de la playlist, y otro método para que nos muestre por pantalla todo el contenido bien formateado.

---
+++ playlist.py
@@ -0,0 +1,41 @@
+class Playlist:
+    def __init__(self,nombre_playlist):
+            self.nombre_playlist=nombre_playlist
+            self.pistas=[]
+
+    # Añade una pista a la lista si no estaba
+    def añadir_pista(self,pista):
+        if pista not in self.pistas:
+            self.pistas.append(pista)
+
+    # Elimina una pista de la lista si estaba
+    def eliminar_pista(self,pista):
+        if pista in self.pistas:
+            self.pistas.remove(pista)
+
+    def calcular_duracion_playlist(self):
+        duracion_t = 0
+        for cancion in self.pistas:
+            duracion_t+=cancion.duracion
+        return duracion_t
+
+    def mostrar_playlist(self):
+        if not self.pistas:
+            return f'Nombre de la playlist: {self.nombre_playlist}\n no tiene contenido.'
+        pistas_playlist=''
+        for pista in self.pistas:
+            pistas_playlist+=f'{pista.mostrar()}\n'
+        return f'Nombre de la playlist: {self.nombre_playlist}\n Canciones: \n{pistas_playlist}'
+
+    # Mezcla dos playlists sin duplicados
+    def __add__(self, otra_playlist):
+        nueva = Playlist(f'Mezcla las playlists: {self.nombre_playlist} y {otra_playlist.nombre_playlist}')
+        nueva.pistas = self.pistas.copy()
+        for pista in otra_playlist.pistas:
+            if pista not in nueva.pistas:
+                nueva.pistas.append(pista)
+        return nueva
+
+    # Compara dos playlists
+    def __eq__(self, otra_playlist):
+        return self.pistas == otra_playlist.pistas
