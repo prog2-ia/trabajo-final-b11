fb9156b0cda20ebe72fd12f19c611c7fe215bf5c
alaafkk <anfb1@alu.ua.es>
2026-03-23 01:00:25 +0100

    En este archivo he creado la clase Biblioteca, que funciona como el almacén o catálogo central de nuestra app. Básicamente, arranca con un nombre y prepara dos listas vacías: una para guardar todos los audios disponibles y otra para las listas de reproducción. Le he programado las funciones necesarias para ir añadiendo pistas al catálogo general, crear playlists nuevas y poder mostrar por pantalla todo el contenido que tenemos guardado

---
+++ biblioteca.py
@@ -0,0 +1,22 @@
+from playlist import Playlist
+
+class Biblioteca:
+    def __init__(self, nombre_biblioteca):
+        self.nombre = nombre_biblioteca
+        self.catalogo = []
+        self.playlists = []
+
+    def agregar_al_catalogo(self, elemento):
+        self.catalogo.append(elemento)
+        return f'{elemento.titulo} se ha añadido al catálogo general.'
+
+    def crear_playlist(self, nombre_playlist):
+        nueva_playlist = Playlist(nombre_playlist)
+        self.playlists.append(nueva_playlist)
+        return nueva_playlist
+
+    def mostrar_catalogo(self):
+        elementos_biblioteca=''
+        for elemento in self.catalogo:
+            elementos_biblioteca += f'{elemento.mostrar()}\n'
+        return elementos_biblioteca
