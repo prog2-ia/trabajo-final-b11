b5b01fdb552295d0702715c31ab505f7e5645e4d
alaafkk <anfb1@alu.ua.es>
2026-05-08 11:44:55 +0200

    Sugerencia de tipos en playlist: añadidos sugerencia de tipos a la clase Playlist
    Sigo añadiendo los tipos de datos, esta vez en el gestor de playlists. Lo mejor aquí es que ya le estamos diciendo explícitamente que las funciones de añadir o quitar solo aceptan objetos de tipo 'ElementoMultimedia'. También he puesto sugerencia de tipos en los métodos mágicos como la suma de playlists y la comparación.

--- playlist.py
+++ playlist.py
@@ -1,34 +1,34 @@
 class Playlist:
-    def __init__(self,nombre_playlist):
-            self.nombre_playlist=nombre_playlist
-            self.pistas=[]
+    def __init__(self, nombre_playlist: str) -> None:
+            self.nombre_playlist = nombre_playlist
+            self.pistas = []
 
     # Añade una pista a la lista si no estaba
-    def añadir_pista(self,pista):
+    def añadir_pista(self,pista: ElementoMultimedia) -> None:
         if pista not in self.pistas:
             self.pistas.append(pista)
 
     # Elimina una pista de la lista si estaba
-    def eliminar_pista(self,pista):
+    def eliminar_pista(self,pista:ElementoMultimedia) -> None:
         if pista in self.pistas:
             self.pistas.remove(pista)
 
-    def calcular_duracion_playlist(self):
+    def calcular_duracion_playlist(self) -> int:
         duracion_t = 0
         for cancion in self.pistas:
-            duracion_t+=cancion.duracion
+            duracion_t += cancion.duracion
         return duracion_t
 
-    def mostrar_playlist(self):
+    def mostrar_playlist(self) -> str:
         if not self.pistas:
             return f'Nombre de la playlist: {self.nombre_playlist}\n no tiene contenido.'
         pistas_playlist=''
         for pista in self.pistas:
-            pistas_playlist+=f'{pista.mostrar()}\n'
+            pistas_playlist += f'{pista.mostrar()}\n'
         return f'Nombre de la playlist: {self.nombre_playlist}\n Canciones: \n{pistas_playlist}'
 
     # Mezcla dos playlists sin duplicados
-    def __add__(self, otra_playlist):
+    def __add__(self, otra_playlist: Self) -> Self:
         nueva = Playlist(f'Mezcla las playlists: {self.nombre_playlist} y {otra_playlist.nombre_playlist}')
         nueva.pistas = self.pistas.copy()
         for pista in otra_playlist.pistas:
@@ -37,5 +37,5 @@ class Playlist:
         return nueva
 
     # Compara dos playlists
-    def __eq__(self, otra_playlist):
+    def __eq__(self, otra_playlist: Any) -> bool:
         return self.pistas == otra_playlist.pistas
