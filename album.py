b4f2134222d85e67ea332a75f77a17c10277ef24
alaafkk <anfb1@alu.ua.es>
2026-05-08 12:06:11 +0200

    Seguimos con lista la clase Album y añadido un import
    Continúo con el tipado del código. En este commit he dejado listos los tipos de la clase Album, indicando que recibe un 'Artista' y que maneja 'Canciones'. Además, me di cuenta de que faltaba el import de Cancion, así que ya está añadido y funcionando. He cambiado el self.canciones añadiendole list[Cancion] para que sea una lista que contiene elementos de tipo Cancion

--- album.py
+++ album.py
@@ -1,21 +1,21 @@
 from perfiles import Artista
-
+from tipos_audio import Cancion
 
 class Album:
-    def __init__(self, titulo, año_lanzamiento, artista):
+    def __init__(self, titulo: str, año_lanzamiento: int, artista: Artista) -> None:
         self.titulo = titulo
         self.año_lanzamiento = año_lanzamiento
-        self.canciones = []
+        self.canciones: list[Cancion] = []
 
         if isinstance(artista, Artista):
             self.artista_creador = artista
         else:
             self.artista_creador = Artista('Artista Desconocido', 'Desconocido')
 
-    def agregar_cancion(self, cancion):
+    def agregar_cancion(self, cancion: Cancion) -> None:
         self.canciones.append(cancion)
 
-    def mostrar_album(self):
+    def mostrar_album(self) -> str:
         texto_album = f'Álbum: {self.titulo} ({self.año_lanzamiento}) - Por: {self.artista_creador.nombre}\n'
         texto_album += 'Pistas:\n'
         numero = 1
