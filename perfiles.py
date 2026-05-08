398185a8c1670ef25ffd752cd4be021232456005
alaafkk <anfb1@alu.ua.es>
2026-05-08 11:57:54 +0200

    Seguimos con sugerencia de tipos: clases Usuario y Artista
    En este caso ha sido más sencillo: he definido los tipos básicos (textos y números enteros) para los datos de los usuarios y los artistas. También he dejado claro que sus funciones de 'mostrar_perfil' devuelven una cadena de texto.

--- perfiles.py
+++ perfiles.py
@@ -1,16 +1,16 @@
 class Usuario:
-    def __init__(self, nombre, gmail):
+    def __init__(self, nombre: str, gmail: str) -> None:
         self.nombre = nombre
         self.gmail = gmail
-    def mostrar_perfil(self):
+    def mostrar_perfil(self) -> str:
         return f'Perfil de Usuario | Nombre: {self.nombre} | Correo: {self.gmail}'
 
 class Artista:
-    def __init__(self, nombre, genero_principal):
+    def __init__(self, nombre: str, genero_principal: str) -> None:
         self.nombre = nombre
         self.genero_principal = genero_principal
-        self.oyentes_mensuales= 0
-    def actualizar_oyentes(self,cantidad):
+        self.oyentes_mensuales = 0
+    def actualizar_oyentes(self,cantidad: int) -> None:
         self.oyentes_mensuales = cantidad
-    def mostrar_perfil(self):
+    def mostrar_perfil(self) -> str:
         return f' Artista: {self.nombre} | Género: {self.genero_principal} | Oyentes: {self.oyentes_mensuales}'
