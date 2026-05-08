f768516fd5a2ed43b5fc99f691afdbabc88c28a0
alaafkk <anfb1@alu.ua.es>
2026-05-08 11:36:33 +0200

    Siguiendo con las sugerencia de tipos en tipos_audio: he añadido sugerencia de tipos a Cancion y Podcast
    Igual que hice con ElementoMultimedia, le he puesto los tipos de datos a las clases hijas. He dejado tipados los atributos nuevos (artista, álbum, tema, etc.) y lo que devuelve la función mostrar. Así nos queda todo el bloque estandarizado.

--- tipos_audio.py
+++ tipos_audio.py
@@ -1,23 +1,23 @@
 from elemento_multimedia import ElementoMultimedia
 
 class Cancion(ElementoMultimedia):
-    def __init__(self,titulo,duracion,fecha_lanzamiento,artista,album,genero):
+    def __init__(self, titulo: str, duracion: int, fecha_lanzamiento: int, artista: str, album: str, genero: str) -> None:
         super().__init__(titulo, duracion, fecha_lanzamiento)
         self.artista = artista
         self.album = album
         self.genero = genero
 
-    def mostrar(self):
-        mostrar_elemento=super().mostrar()
+    def mostrar(self) -> str:
+        mostrar_elemento = super().mostrar()
         return f'{mostrar_elemento} del artista {self.artista} y el album {self.album}. Genero: {self.genero}'
 
 class Podcast(ElementoMultimedia):
-    def __init__(self,titulo,duracion,fecha_lanzamiento,presentador,num_episodio,tema):
+    def __init__(self, titulo: str, duracion: int, fecha_lanzamiento: int, presentador: str, num_episodio: int, tema: str)-> None:
         super().__init__(titulo, duracion, fecha_lanzamiento)
         self.presentador = presentador
         self.num_episodio = num_episodio
         self.tema = tema
 
-    def mostrar(self):
-        mostrar_elemento=super().mostrar()
+    def mostrar(self) -> str:
+        mostrar_elemento = super().mostrar()
         return f'{mostrar_elemento} episodio {self.num_episodio} sobre {self.tema} del presentador {self.presentador}'
