50d22b0c6eb291cbfa24a349eded4e61cb183e52
alaafkk <anfb1@alu.ua.es>
2026-05-08 11:53:16 +0200

    Siguiendo con los type hints: clase Biblioteca
    Sigo añadiendo los tipos de datos al proyecto. Ahora le ha tocado a la clase Biblioteca. He dejado indicado que al catálogo solo entran 'ElementosMultimedia', qué devuelve cada función de mostrar, y he especificado que 'crear_playlist' devuelve directamente un objeto 'Playlist'.

--- biblioteca.py
+++ biblioteca.py
@@ -1,21 +1,21 @@
 from playlist import Playlist
 
 class Biblioteca:
-    def __init__(self, nombre_biblioteca):
+    def __init__(self, nombre_biblioteca: str) ->None:
         self.nombre = nombre_biblioteca
         self.catalogo = []
         self.playlists = []
 
-    def agregar_al_catalogo(self, elemento):
+    def agregar_al_catalogo(self, elemento: ElementoMultimedia) -> str:
         self.catalogo.append(elemento)
         return f'{elemento.titulo} se ha añadido al catálogo general.'
 
-    def crear_playlist(self, nombre_playlist):
+    def crear_playlist(self, nombre_playlist: str) -> Playlist:
         nueva_playlist = Playlist(nombre_playlist)
         self.playlists.append(nueva_playlist)
         return nueva_playlist
 
-    def mostrar_catalogo(self):
+    def mostrar_catalogo(self) -> str:
         elementos_biblioteca=''
         for elemento in self.catalogo:
             elementos_biblioteca += f'{elemento.mostrar()}\n'
