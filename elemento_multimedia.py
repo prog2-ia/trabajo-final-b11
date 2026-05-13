5c5b4c7e38747aa5310fdb7f360b79522f119f06
Javier Pastor Gómez <jpg136@alu.ua.es>
2026-05-13 14:09:40 +0200

    Más cambios en el manejo de excepciones.

--- elemento_multimedia.py
+++ elemento_multimedia.py
@@ -2,14 +2,14 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 class ElementoMultimedia(ABC):
-    def __init__(self,titulo: str,duracion: int,fecha_lanzamiento: int) -> None:
+    def __init__(self,titulo: str,duracion: int | float,fecha_lanzamiento: int) -> None:
         self.titulo = titulo
         self.duracion = duracion
-        self.fecha_lanzamiento = fecha_lanzamiento
 
         #Impedimos que la fecha de lanzamiento sea negativa
         if fecha_lanzamiento < 0:
             raise ValueError('La fecha de lanzamiento no puede ser negaiva')
+        self.fecha_lanzamiento = fecha_lanzamiento
 
     @property
     def duracion(self) -> int | float:
@@ -18,9 +18,6 @@ class ElementoMultimedia(ABC):
     # Si duracion es menor de 0, se asocia un valor predeterminado 3.0.
     @duracion.setter
     def duracion(self, valor: int | float) -> None:
-        # Manejamos un posible error de tipo de dato
-        if not isinstance(valor, int | float):
-            raise TypeError('El tipo de dato debe ser "int" o "float"')
         if valor <= 0:
             self._duracion = 3.0
         else:
@@ -34,6 +31,7 @@ class ElementoMultimedia(ABC):
     # Compara si dos pistas son iguales
     def __eq__(self, otro: Any) -> bool:
         if isinstance(otro, ElementoMultimedia):
-            return self.titulo == otro.titulo and self.duracion == otro.duracion
-        return False
+            raise TypeError('Tipo de datos inválido, debe ser "ElementoMultimedia")
+        return self.titulo == otro.titulo and self.duracion == otro.duracion
+        
 
