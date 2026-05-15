64c4cfd2e2ade7a603fc4ae242f476640f8a93ac
Buccipan <jpg136@alu.ua.es>
2026-05-15 09:23:42 +0200

    Arreglado un error.

--- perfiles.py
+++ perfiles.py
@@ -12,15 +12,13 @@ class Artista:
     def __init__(self, nombre: str, genero_principal: str) -> None:
         if not nombre.strip():
             raise ValueError('El nombre del artista no puede estar vacío')
-        if oyentes_mensuales < 0:
-            raise ValueError('Los oyentes no pueden ser negativos')
         self.nombre = nombre
         self.genero_principal = genero_principal
         self.oyentes_mensuales = 0
         
     def actualizar_oyentes(self,cantidad: int) -> None:
         if cantidad < 0:
-            raise ValueError('La cantidad de oyentes no puede ser negativa'
+            raise ValueError('La cantidad de oyentes no puede ser negativa')
         self.oyentes_mensuales = cantidad
         
     def mostrar_perfil(self) -> str:
